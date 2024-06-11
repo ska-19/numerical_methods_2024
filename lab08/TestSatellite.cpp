60f64181dca294792abf5f82f8100b5519a12962
Sosnin Timofey <90689276+cactus-tim@users.noreply.github.com>
2024-06-11 16:22:37 +0300

    fixed

--- lab08/TestSatellite.cpp
+++ lab08/TestSatellite.cpp
@@ -2,17 +2,21 @@
 #include "Everhart.h"
 #include "SatelliteEq.h"
 
+using namespace ADAI;
+
 template<typename T>
 void runTest() {
     T dt = static_cast<T>(10.0);
     T t_end = static_cast<T>(86400.0);
-    std::vector<T> initial_conditions = {static_cast<T>(7000e3), static_cast<T>(0.0), static_cast<T>(0.0), static_cast<T>(7.12e3)}; // Начальные условия: {x, y, vx, vy}
+    std::vector<T> initial_conditions = {static_cast<T>(7000e3), static_cast<T>(0.0), static_cast<T>(0.0),
+                                         static_cast<T>(0.0), static_cast<T>(7.12e3), static_cast<T>(1.0e3)};
 
-    ADAI::Everhart<T> solver(dt, initial_conditions, ADAI::satelliteEquations<T>);
+    Everhart<T> solver(dt, initial_conditions, satelliteEquations<T>);
     std::vector<std::vector<T>> trajectory = solver.integrate(t_end);
 
-    for (const auto& state : trajectory) {
-        std::cout << "x: " << state[0] << ", y: " << state[1] << ", vx: " << state[2] << ", vy: " << state[3] << std::endl;
+    for (const auto &state: trajectory) {
+        std::cout << "x: " << state[0] << ", y: " << state[1] << ", z: " << state[2]
+                  << ", vx: " << state[3] << ", vy: " << state[4] << ", vz: " << state[5] << std::endl;
     }
 }
 
